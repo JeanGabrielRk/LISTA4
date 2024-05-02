fd36f54f9e6f90cc2e2a472875742b454f53a9d9
JeanGabrielRk <jeangabriel058@gmail.com>
2024-05-02 09:20:26 -0300

    Cenario 4

--- Cenario04/src/br/edu/up/Programa.java
+++ Cenario04/src/br/edu/up/Programa.java
@@ -2,7 +2,9 @@ import controles.Controlador;
 
 public class Programa {
     public static void main(String[] args) throws Exception {
+
         Controlador controlador = new Controlador();
         controlador.iniciar();
+        
     }
 }
